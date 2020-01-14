1e530e8e4035ee7c0fb4f2f40587f1f0c8779318
aliffiansyah <mochammadaliffiansyah@gmail.com>
2020-01-14 22:26:47 +0700

    list dan tangkap tabel tingkat kepuasan masyarakat

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -9,6 +9,8 @@ class Form extends CI_Controller{
 
     $this->load->model('model_form');
     //Codeigniter : Write Less Do More
+    $this->load->model('model_tingkatKepuasanPelanggan');
+    $this->load->model('model_tingkatKepuasanPelangganQuestion');
   }
 
   function index()
@@ -23,15 +25,40 @@ class Form extends CI_Controller{
 
   function show(){
      $data['form'] = $this->model_form->showForm($this->uri->segment(3))->row_array();
-     //print_r($data['form']);
+     $data['TingkatKepuasanPelanggan'] = $this->model_tingkatKepuasanPelanggan->listTingkatKepuasanPelanggan($this->uri->segment(3))->result();
+     $i=0;
+     foreach ($data['TingkatKepuasanPelanggan'] as $key) {
+         $data['data'][$i]['count'] = $this->model_tingkatKepuasanPelangganQuestion->countlistQuestion($key->ID_TKM);
+         echo "find questi where id tkm = ".$key->ID_TKM;
+         $data['data'][$i]['question'] = $this->model_tingkatKepuasanPelangganQuestion->listQuestion($key->ID_TKM)->result();
+         $i++;
+     }
+     print_r($data);
      $this->load->view('template/Header');
      $this->load->view('form/Header',$data);
 
 
 
-
+     $this->load->view('form/TingkatKepuasanPelanggan',$data);
      $this->load->view('form/Footer');
      $this->load->view('template/Footer');
+  }
+
+  public function simpan()
+  {
+
+      ///// bab 2 /////
+      $jumlahBab = $this->input->post('jumlahBab');
+      echo "jumlah bab".$jumlahBab."-----";
+      for ($i=0; $i < $jumlahBab ; $i++) {
+          echo "<br>";
+          $jumlahPertanyaan[$i] = $this->input->post('jumlahQuestion'.$i);
+          for ($j=0; $j <$jumlahPertanyaan[$i] ; $j++) {
+              echo $this->input->post('answer'.$i.$j);
+          }
+      }
+      // echo $count;
+
   }
 
 }
