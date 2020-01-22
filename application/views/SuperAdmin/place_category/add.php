659fecf8abed1441c8c74aef2378e3ff02b3be01
rook-ies <astinarsenal@gmail.com>
2020-01-22 09:12:19 +0700

    Place category view

--- application/views/SuperAdmin/place_category/add.php
+++ application/views/SuperAdmin/place_category/add.php
@@ -1,11 +1,22 @@
-<?php echo form_open('place_category/add'); ?>
-
-	<div>
-		<span class="text-danger">*</span>CATEGORY :
-		<input type="text" name="CATEGORY" value="<?php echo $this->input->post('CATEGORY'); ?>" />
-		<span class="text-danger"><?php echo form_error('CATEGORY');?></span>
-	</div>
-
-	<button type="submit">Save</button>
-
-<?php echo form_close(); ?>
+<div class="col-md-12 d-flex align-items-stretch grid-margin">
+  <div class="row flex-grow">
+    <div class="col-12 stretch-card">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title"><?php echo $title ?></h4>
+          <?php echo form_open('place_category/add'); ?>
+            <div class="form-group row">
+              <label for="exampleInputEmail2" class="col-sm-3 col-form-label">CATEGORY</label>
+              <div class="col-sm-9">
+                <input type="text" class="form-control" name="CATEGORY" value="<?php echo $this->input->post('CATEGORY'); ?>">
+								<span class="text-danger"><?php echo form_error('CATEGORY');?></span>
+							</div>
+            </div>
+            <button type="submit" class="btn btn-success mr-2">Save</button>
+            <?php echo anchor(base_url('place_category'), 'Cancel'); ?>
+          <?php echo form_close(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- application/views/SuperAdmin/place_category/index.php
+++ application/views/SuperAdmin/place_category/index.php
@@ -1,17 +1,33 @@
-<table border="1" width="100%">
-    <tr>
-		<th>ID PLACE CATEGORY</th>
-		<th>CATEGORY</th>
-		<th>Actions</th>
-    </tr>
-	<?php foreach($place_category as $P){ ?>
-    <tr>
-		<td><?php echo $P['ID_PLACE_CATEGORY']; ?></td>
-		<td><?php echo $P['CATEGORY']; ?></td>
-		<td>
-            <a href="<?php echo site_url('place_category/edit/'.$P['ID_PLACE_CATEGORY']); ?>">Edit</a> |
-            <a href="<?php echo site_url('place_category/remove/'.$P['ID_PLACE_CATEGORY']); ?>">Delete</a>
-        </td>
-    </tr>
-	<?php } ?>
-</table>
+<div class="row">
+  <div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+          <h4 class="card-title"><?php echo $title; ?></h4>
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>ID PLACE CATEGORY</th>
+              		<th>CATEGORY</th>
+              		<th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach($place_category as $P){ ?>
+                  <tr>
+                  <td><?php echo $P['ID_PLACE_CATEGORY']; ?></td>
+                  <td><?php echo $P['CATEGORY']; ?></td>
+                  <td>
+                          <a href="<?php echo site_url('place_category/edit/'.$P['ID_PLACE_CATEGORY']); ?>">Edit</a> |
+                          <a href="<?php echo site_url('place_category/remove/'.$P['ID_PLACE_CATEGORY']); ?>">Delete</a>
+                      </td>
+                  </tr>
+                <?php } ?>
+              </tbody>
+            </table>
+             <a href=<?php echo base_url('place_category/add') ?>><button class="btn btn-success mr-2">Tambah</button></a>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
