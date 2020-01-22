c2178063c6c20a258399cc7e496b417242cdd007
rook-ies <astinarsenal@gmail.com>
2020-01-22 10:03:24 +0700

    Place view

--- application/views/SuperAdmin/place/edit.php
+++ application/views/SuperAdmin/place/edit.php
@@ -1,41 +1,52 @@
-<?php echo form_open('place/edit/'.$place['ID_PLACE']); ?>
+<div class="col-12 stretch-card">
+  <div class="card">
+	  <div class="card-body">
+        <h4 class="card-title"><?php echo $title ?></h4>
+		<?php echo form_open('place/edit/'.$place['ID_PLACE']); ?>
+		  <div class="form-group row">
+	          <label for="exampleFormControlSelect2" class="col-sm-3 col-form-label">Place Type</label>
+			  <div class="col-sm-9">
+		          <select class="form-control" id="exampleFormControlSelect2" name="ID_PLACE_TYPE">
+		            <option value="">select place_type</option>
+								<?php
+								foreach($all_place_type as $place_type)
+								{
+									$selected = ($place_type['ID_PLACE_TYPE'] == $this->input->post('ID_PLACE_TYPE')) ? ' selected="selected"' : "";
 
-	<div>
-		<span class="text-danger">*</span>Place Type :
-		<select name="ID_PLACE_TYPE">
-			<option value="">select place_type</option>
-			<?php
-			foreach($all_place_type as $place_type)
-			{
-				$selected = ($place_type['ID_PLACE_TYPE'] == $place['ID_PLACE_TYPE']) ? ' selected="selected"' : "";
+									echo '<option value="'.$place_type['ID_PLACE_TYPE'].'" '.$selected.'>'.$place_type['TYPE'].'</option>';
+								}
+								?>
+		          </select>
+							<span class="text-danger"><?php echo form_error('ID_PLACE_TYPE');?></span>
+		  		</div>
+      </div>
 
-				echo '<option value="'.$place_type['ID_PLACE_TYPE'].'" '.$selected.'>'.$place_type['TYPE'].'</option>';
-			}
-			?>
-		</select>
-		<span class="text-danger"><?php echo form_error('ID_PLACE_TYPE');?></span>
-	</div>
-	<div>
-		<span class="text-danger">*</span>Place Category :
-		<select name="ID_PLACE_CATEGORY">
-			<option value="">select place_category</option>
-			<?php
-			foreach($all_place_category as $place_category)
-			{
-				$selected = ($place_category['ID_PLACE_CATEGORY'] == $place['ID_PLACE_CATEGORY']) ? ' selected="selected"' : "";
+			<div class="form-group row">
+	          <label for="exampleFormControlSelect2" class="col-sm-3 col-form-label">Place Category</label>
+			  <div class="col-sm-9">
+		          <select class="form-control" id="exampleFormControlSelect2" name="ID_PLACE_CATEGORY">
+		            <option value="">select place category</option>
+								<?php
+								foreach($all_place_category as $place_category)
+								{
+									$selected = ($place_category['ID_PLACE_CATEGORY'] == $this->input->post('ID_PLACE_CATEGORY')) ? ' selected="selected"' : "";
 
-				echo '<option value="'.$place_category['ID_PLACE_CATEGORY'].'" '.$selected.'>'.$place_category['CATEGORY'].'</option>';
-			}
-			?>
-		</select>
-		<span class="text-danger"><?php echo form_error('ID_PLACE_CATEGORY');?></span>
-	</div>
-	<div>
-		<span class="text-danger">*</span>NAMA :
-		<input type="text" name="NAMA" value="<?php echo ($this->input->post('NAMA') ? $this->input->post('NAMA') : $place['NAMA']); ?>" />
-		<span class="text-danger"><?php echo form_error('NAMA');?></span>
-	</div>
-
-	<button type="submit">Save</button>
-
-<?php echo form_close(); ?>
+									echo '<option value="'.$place_category['ID_PLACE_CATEGORY'].'" '.$selected.'>'.$place_category['CATEGORY'].'</option>';
+								}
+								?>
+							</select>
+							<span class="text-danger"><?php echo form_error('ID_PLACE_CATEGORY');?></span>
+		  		</div>
+      </div>
+			<div class="form-group row">
+              <label for="exampleInputEmail2" class="col-sm-3 col-form-label">NAMA</label>
+              <div class="col-sm-9">
+                <input class="form-control" type="text" name="NAMA" value="<?php echo $this->input->post('NAMA'); ?>" />
+								<span class="text-danger"><?php echo form_error('NAMA');?></span>
+							</div>
+          <button type="submit" class="btn btn-success mr-2">Submit</button>
+		  <?php echo anchor(base_url('place'), 'Cancel'); ?>
+		 <?php echo form_close(); ?>
+      </div>
+  </div>
+</div>
