2984b9d60b3662dfe0b1cc4d86b8cde83c2aa92f
Mochammad Aliffiansyah <44664750+aliffiansyahm@users.noreply.github.com>
2020-02-02 21:49:07 +0700

    Update and rename dashboardAdminBiasa.php to DashboardAdminBiasa.php

--- application/controllers/DashboardAdminBiasa.php
+++ application/controllers/DashboardAdminBiasa.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class dashboardAdminBiasa extends CI_Controller{
+class DashboardAdminBiasa extends CI_Controller{
 
     public function __construct()
     {
